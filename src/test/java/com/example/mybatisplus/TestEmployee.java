6ef8efe605a01cba7b10e3a9294e3ddadb65deff
zrj-coder <854530110@qq.com>
2020-07-12 21:11:35 +0800

    mybatis-plus常用方法

--- src/test/java/com/example/mybatisplus/TestEmployee.java
+++ src/test/java/com/example/mybatisplus/TestEmployee.java
@@ -1,5 +1,6 @@
 package com.example.mybatisplus;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -11,7 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName TestMP
@@ -51,14 +55,20 @@ public class TestEmployee {
     public void testUpdate() {
         System.out.println( ("----- update method test ------") );
         Employee employee = new Employee();
-        employee.setId( 6 );
-        employee.setLastName( "hello" );
+        employee.setId( 1 );
+        employee.setLastName( "mybatis" );
         employee.setAge( 32 );
         employee.setGender( 0 );
-        employee.setEmail( "hello@163.com" );
+        employee.setEmail( "mybatis@163.com" );
 
-        int insert = employeeMapper.updateById( employee );
-        System.out.println( "更新行数" + insert );
+        //会进行非空判断
+        //int insert = employeeMapper.updateById( employee );
+        //System.out.println( "按Id更新行数" + insert );
+
+        //按条件更新
+        QueryWrapper<Employee> queryWrapper = new QueryWrapper<Employee>().eq( "age", "66" );
+        int insertWrp = employeeMapper.update( employee, queryWrapper );
+        System.out.println( "按条件更新行数" + insertWrp );
     }
 
     /**
@@ -90,9 +100,35 @@ public class TestEmployee {
     @Test
     public void testSelect() {
         System.out.println( ("----- selectAll method test ------") );
+
+        /*//查询所有
         List<Employee> employeeList = employeeMapper.selectList( null );
-        //Assert.assertEquals( 5, employeeList.size() );
         employeeList.forEach( System.out::println );
+
+        //根据id查询
+        Employee employee = employeeMapper.selectById( 1 );
+        System.out.println( "根据id查询：" + employee );
+
+        //根据id集合查询
+        List<Integer> idList = new ArrayList( 10 );
+        idList.add( 1 );
+        idList.add( 2 );
+        List employees = employeeMapper.selectBatchIds( idList );
+        employees.forEach( System.out::println );
+
+        //通过多个列查询,eq的值只能是数据库字段值，否则报错
+        //根据条件只能返回一条，否则报错
+        Wrapper<Employee> wrapper = new QueryWrapper<Employee>().eq( "last_name", "bear" );
+        Employee employee1 = employeeMapper.selectOne( wrapper );
+        System.out.println( "employee：" + employee1 );*/
+
+        //通过map查询 Map<表字段名, 值>
+        Map<String, Object> columnMap = new HashMap<>( 16 );
+        //columnMap.put( "last_name", "aliyun" );
+        columnMap.put( "age", 20 );
+        List<Employee> employeeList1 = employeeMapper.selectByMap( columnMap );
+        employeeList1.forEach( System.out::println );
+
     }
 
     /**
@@ -106,11 +142,11 @@ public class TestEmployee {
         int pageSize = 2;
         Page<Employee> page = new Page<>( pageNum, pageSize );
         QueryWrapper<Employee> queryWrapper = new QueryWrapper<Employee>().gt( "age", "20" );
-        //QueryWrapper<Employee> queryWrapper = new QueryWrapper<Employee>().gt( "age", "20" );
-        IPage<Employee> mpUserIPage = employeeMapper.selectPage( page, queryWrapper );
+        Page<Employee> userIPage = employeeMapper.selectPage( page, null );
 
-        System.out.println( "总计：" + mpUserIPage.getSize() );
-        mpUserIPage.getRecords().forEach( System.out::println );
+        //内存分页，假分页，不能分页
+        System.out.println( "总计：" + userIPage.getSize() );
+        userIPage.getRecords().forEach( System.out::println );
     }
 
 
